29f736c3e390fa4514387796a7df2a355b8e3dfb
Patrick Alexis <alexispatrick77@gmail.com>
2023-04-10 21:14:50 -0700

    Update bottom nav item icons

--- app/src/main/java/com/example/gpt/MainActivity.kt
+++ app/src/main/java/com/example/gpt/MainActivity.kt
@@ -5,14 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -53,8 +56,13 @@ fun ShowApp() {
                 NavigationBar {
                     bottomNavItems.forEach { screen ->
                         NavigationBarItem(
-                            icon = { screen.iconId },
-                            label = { screen.route },
+                            icon = {
+                                Icon(
+                                    painter = painterResource(id = screen.icon),
+                                    contentDescription = screen.route
+                                )
+                            },
+                            label = { Text(screen.route) },
                             selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
                             onClick = { navController.navigate(screen.route) }
                         )

10a478830d9bbc5ca25a26e28400b2b936fb36a4
Patrick Alexis <alexispatrick77@gmail.com>
2023-05-19 08:50:45 -0700

    renamed image request models

--- app/src/main/java/com/example/gpt/data/model/image/ImageCreateRequest.kt
+++ app/src/main/java/com/example/gpt/data/model/image/ImageCreateRequest.kt
@@ -1,6 +1,6 @@
 package com.example.gpt.data.model.image
 
-data class CreateImageRequest(
+data class ImageCreateRequest(
     val prompt: String,
     val n: Int,
     val size: String = "512x512"
--- app/src/main/java/com/example/gpt/data/repository/image/ImageRepository.kt
+++ app/src/main/java/com/example/gpt/data/repository/image/ImageRepository.kt
@@ -1,9 +1,9 @@
 package com.example.gpt.data.repository.image
 
-import com.example.gpt.data.model.image.CreateImageRequest
+import com.example.gpt.data.model.image.ImageCreateRequest
 import com.example.gpt.data.model.image.ImageMessage
 import kotlinx.coroutines.flow.Flow
 
 interface ImageRepository {
-    fun getImages(createImageRequest: CreateImageRequest): Flow<ImageMessage>
+    fun getImages(imageCreateRequest: ImageCreateRequest): Flow<ImageMessage>
 }
